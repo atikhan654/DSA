8abae0a1f1b4ddd001ea0dc4aa97ac87891990c9
Atiullah Khan <atikhan654@gmail.com>
2024-11-17 23:55:10 +0530

    Comment added in Bubble and Insertion sort file

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -2,25 +2,32 @@ package sorting;
 
 import java.util.Arrays;
 
-// push the maximum at last
-// use adjustment swapping
+/**
+ * pushes the maximum to the last, by doing adjustment swap
+ * Worst,Average time complexity -> O(n2),
+ * and Best case O(n) {Do not forget to add break else it will run till O(n2)}
+ */
 public class BubbleSort {
   public static void main(String[] args) {
     int[] arr = {13, 46, 9, 52, 20, 6};
-    bubbleSort(arr, arr.length);
+    bubbleSort(arr);
     System.out.println("After Bubble Sorting: " + Arrays.toString(arr));
   }
 
-  private static void bubbleSort(int[] arr, int n) {
+  private static void bubbleSort(int[] arr) {
+    int n = arr.length;
     for (int i = n - 1; i >= 1; i--) {
+      int didSwap = 0;
       for (int j = 0; j <= i - 1; j++) {
         if (arr[j] > arr[j + 1]) {
-          // swap
+          // swapping
           int temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
+          didSwap = 1;
         }
       }
+      if (didSwap == 0) break;
     }
   }
 }
--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -2,22 +2,25 @@ package sorting;
 
 import java.util.Arrays;
 
+/** Place the element and place it in its correct order
+ * Worst and Average case - O(n2), Best case O(n)
+ * */
 public class InsertionSort {
   public static void main(String[] args) {
     int[] arr = {9, 12, 14, 6, 15, 8, 13};
-    insertionSort(arr, arr.length);
+    insertionSort(arr);
     System.out.println("After Insertion Sort: " + Arrays.toString(arr));
   }
 
-  private static void insertionSort(int[] arr, int n) {
+  private static void insertionSort(int[] arr) {
+    int n = arr.length;
     for (int i = 0; i <= n - 1; i++) {
       int j = i;
-      while (j > 0 && arr[j] < arr[j - 1]) {
+      while (j > 0 && arr[j - 1] > arr[j]) {
         // swap
-        int temp = arr[j];
-        arr[j] = arr[j - 1];
-        arr[j - 1] = temp;
-
+        int temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
         j--;
       }
     }
